8ec04a91100bab7ecf4be1de4eaf83842d940713
Jan Schuessler <jan.schuessler@fhgr.ch>
2022-09-28 23:29:31 +0200

    updated unittest for spm fleet

--- test_ships.py
+++ test_ships.py
@@ -9,31 +9,37 @@ class TestShips(unittest.TestCase):
         my_fleet.ships['destroyer_1'].set_start_coordinates([7,7])
         my_fleet.place_ships()
 
-        spm = my_fleet.ships['carrier_1'].shots_per_minute
+        spm_fleet = my_fleet.shots_per_minute
+
+        spm_ship = my_fleet.ships['carrier_1'].shots_per_minute
 
         is_hit, is_destroyed = my_fleet.hit_coordinates([2,2])
         self.assertFalse(is_hit)
         self.assertFalse(is_destroyed)
-        self.assertEqual(spm, my_fleet.ships['carrier_1'].shots_per_minute)
+        self.assertEqual(spm_ship, my_fleet.ships['carrier_1'].shots_per_minute)
+        self.assertEqual(spm_fleet, my_fleet.shots_per_minute)
 
         is_hit, is_destroyed = my_fleet.hit_coordinates([2,3])
         self.assertTrue(is_hit)
         self.assertFalse(is_destroyed)
-        self.assertEqual(spm-2, my_fleet.ships['carrier_1'].shots_per_minute)
+        self.assertEqual(spm_ship-2, my_fleet.ships['carrier_1'].shots_per_minute)
+        self.assertEqual(spm_fleet-2, my_fleet.shots_per_minute)
         self.assertFalse(my_fleet.ships['carrier_1'].destroyed)
 
-        spm = my_fleet.ships['destroyer_1'].shots_per_minute
+        spm_ship = my_fleet.ships['destroyer_1'].shots_per_minute
 
         is_hit, is_destroyed = my_fleet.hit_coordinates([7,7])
         self.assertTrue(is_hit)
         self.assertFalse(is_destroyed)
-        self.assertEqual(spm-2, my_fleet.ships['destroyer_1'].shots_per_minute)
+        self.assertEqual(spm_ship-2, my_fleet.ships['destroyer_1'].shots_per_minute)
+        self.assertEqual(spm_fleet-4, my_fleet.shots_per_minute)
         self.assertFalse(my_fleet.ships['destroyer_1'].destroyed)
 
         is_hit, is_destroyed = my_fleet.hit_coordinates([7,8])
         self.assertTrue(is_hit)
         self.assertTrue(is_destroyed)
-        self.assertEqual(spm-4, my_fleet.ships['destroyer_1'].shots_per_minute)
+        self.assertEqual(spm_ship-4, my_fleet.ships['destroyer_1'].shots_per_minute)
+        self.assertEqual(spm_fleet-6, my_fleet.shots_per_minute)
         self.assertEqual(0, my_fleet.ships['destroyer_1'].shots_per_minute)
         self.assertTrue(my_fleet.ships['destroyer_1'].destroyed)
 
